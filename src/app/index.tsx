7522886c72882c404f50be43a235976c878c5da0
JuanaVillalba <juananicolvillalba@gmail.com>
2026-04-14 12:25:55 -0300

    Prototipo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Screen from '@/components/ui/Screen';
 import BrandHeader from '@/components/ui/BrandHeader';
-import logo from "../../assets/logo.png"
+import { useRouter } from 'expo-router';
+import { Button } from 'react-native';
 
 export default function Home() {
+  const router = useRouter();
   return (
     <Screen>
-      <BrandHeader
-        title="Mi App"
-        subtitle="Bienvenido"
-        logo={logo} />
-    </Screen>
+      <Button title="Movies" onPress={()=>router.push("/movies/index")}/>
+      <Button title="Categorias" onPress={()=>router.push("/categorias/index")}/>
+      </Screen>
   );
 }
---
+++ src/app/movies/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Screen from '@/components/ui/Screen';
+import { View } from 'react-native';
+import {Card, Text} from 'react-native-paper';
+
+
+function MoviesScreen(){
+    return(
+    <Screen>
+        <Text>Pantalla de movies</Text>
+        
+    </Screen>
+    )
+}
